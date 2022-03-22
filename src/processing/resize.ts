e906cc9fb4c21611fe54f9075b232c679025f498
Hassuunna <hassanraafat55@gmail.com>
2022-03-22 20:21:41 +0200

    initial commit

---
+++ src/processing/resize.ts
@@ -0,0 +1,47 @@
+import sharp from 'sharp'
+import path from 'path'
+
+async function resizeImage(
+	imageName: string,
+	width: number,
+	height: number,
+	singleDimension: number
+) {
+	const source = `${path.resolve('./')}/assets/${imageName}.jpg`
+	switch (singleDimension) {
+		case 0:
+			await sharp(source)
+				.resize(width, height)
+				.jpeg()
+				.toFile(
+					`${path.resolve(
+						'./'
+					)}/assets/thumbnails/${imageName}_${width}_${height}.jpg`
+				)
+			break
+		case 1:
+			await sharp(source)
+				.resize(height)
+				.jpeg()
+				.toFile(
+					`${path.resolve(
+						'./'
+					)}/assets/thumbnails/${imageName}_${width}_${height}.jpg`
+				)
+			break
+		case 2:
+			await sharp(source)
+				.resize(width)
+				.jpeg()
+				.toFile(
+					`${path.resolve(
+						'./'
+					)}/assets/thumbnails/${imageName}_${width}_${height}.jpg`
+				)
+			break
+		default:
+			break
+	}
+}
+
+export default resizeImage
